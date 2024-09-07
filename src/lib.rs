02e063b56a4fd60cfb801f038bb82889eb2df614
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-09-07 13:47:07 +0500

    refactored routes syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    dev::Server,
+    get, post,
+    web::{self, service},
+    App, HttpResponse, HttpServer, Responder,
+};
 use serde::Deserialize;
 
+#[get("/health-check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
@@ -13,18 +19,15 @@ struct AnnouncementPostData {
     announcement: String,
 }
 
+#[post("/announcement")]
 async fn announce(data: web::Json<AnnouncementPostData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health-check", web::get().to(health_check))
-            .route("/announcement", web::post().to(announce))
-    })
-    .listen(listener)?
-    .run();
+    let server = HttpServer::new(|| App::new().service(health_check).service(announce))
+        .listen(listener)?
+        .run();
 
     Ok(server)
 }
