cda428bdce2ce78883e140ca296fd594b9d48e8b
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-09-12 15:52:45 +0500

    made login more beautiful

--- src/routes/admin_portal/login.rs
+++ src/routes/admin_portal/login.rs
@@ -1,5 +1,8 @@
+use std::time::Duration;
+
 use actix_web::{get, post, HttpResponse};
 use askama::Template;
+use tokio::time::sleep;
 
 #[derive(Template)]
 #[template(path = "login.html")]
@@ -12,5 +15,6 @@ async fn login() -> HttpResponse {
 
 #[post("/submit-login")]
 async fn submit_login() -> HttpResponse {
+    sleep(Duration::from_secs(5)).await;
     HttpResponse::Ok().body("Thank you!")
 }
