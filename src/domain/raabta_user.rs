aa9992342221090bfaa95e1ead517d4a6fe8e126
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-09-17 14:18:35 +0500

    added domain validation

--- src/domain/raabta_user.rs
+++ src/domain/raabta_user.rs
@@ -1,3 +1,7 @@
+use uuid::Uuid;
+
+use crate::routes::UserRole;
+
 enum RaabtaUser {
     Student(Student),
     Parent(UserData),
@@ -27,3 +31,69 @@ struct SchoolAdmin {
 }
 
 impl RaabtaUser {}
+
+pub struct NewUser {
+    pub id: Uuid,
+    pub display_name: DisplayName,
+    pub first_name: UserName,
+    pub last_name: UserName,
+    pub email: UserEmail,
+    pub phone_number: Option<String>,
+    pub user_role: UserRole,
+}
+
+pub struct UserName(String);
+impl UserName {
+    pub fn parse(s: String) -> Result<UserName, String> {
+        let is_empty_or_whitespace = s.trim().is_empty();
+        let is_too_long = s.len() > 64;
+        let has_space = s.contains(" ");
+
+        if is_empty_or_whitespace || is_too_long || has_space {
+            Err(format!("{} is not a valid name", s))
+        } else {
+            let s = s.to_lowercase();
+            let result = match s.chars().next() {
+                Some(first_char) => {
+                    let first_cap = first_char.to_uppercase().to_string();
+                    match s.get(first_char.len_utf8()..) {
+                        Some(rest) => format!("{}{}", first_cap, rest),
+                        None => String::new(),
+                    }
+                }
+                None => String::new(),
+            };
+
+            Ok(Self(result))
+        }
+    }
+}
+impl AsRef<str> for UserName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+pub struct DisplayName(String);
+impl DisplayName {
+    pub fn parse(first_name: &UserName, last_name: &UserName) -> DisplayName {
+        Self(format!("{} {}", first_name.0, last_name.0))
+    }
+}
+impl AsRef<str> for DisplayName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+pub struct UserEmail(String);
+impl UserEmail {
+    pub fn parse(first_name: &UserName) -> UserEmail {
+        Self(format!("{}@riveroaks.com", first_name.0))
+    }
+}
+impl AsRef<str> for UserEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -3,9 +3,11 @@ use serde::Deserialize;
 use sqlx::PgPool;
 use uuid::Uuid;
 
+use crate::domain::{DisplayName, NewUser, UserEmail, UserName};
+
 #[derive(Deserialize, sqlx::Type)]
 #[sqlx(type_name = "UserRole", rename_all = "SCREAMING_SNAKE_CASE")]
-enum UserRole {
+pub enum UserRole {
     Student,
     Parent,
     Teacher,
@@ -19,29 +21,46 @@ struct CreateUserBody {
     role: UserRole,
 }
 
-impl CreateUserBody {
-    fn get_display_name(&self) -> String {
-        format!("{} {}", self.first_name, self.last_name)
-    }
-    fn get_email(&self) -> String {
-        format!("{}@riveroaks.com", self.first_name)
+impl TryFrom<CreateUserBody> for NewUser {
+    type Error = String;
+
+    fn try_from(value: CreateUserBody) -> Result<Self, Self::Error> {
+        let first_name = UserName::parse(value.first_name)?;
+        let last_name = UserName::parse(value.last_name)?;
+        let display_name = DisplayName::parse(&first_name, &last_name);
+        let email = UserEmail::parse(&first_name);
+
+        Ok(Self {
+            id: Uuid::new_v4(),
+            display_name,
+            first_name,
+            last_name,
+            email,
+            phone_number: value.phone_number,
+            user_role: value.role,
+        })
     }
 }
 
 #[post("/user")]
 async fn create_user(body: web::Json<CreateUserBody>, pool: web::Data<PgPool>) -> HttpResponse {
+    let new_user: NewUser = match body.0.try_into() {
+        Ok(value) => value,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
     if let Err(_) = sqlx::query!(
         r#"
         insert into raabta_user (id, display_name, first_name, last_name, email, phone_number, user_role)
         values ($1, $2, $3, $4, $5, $6, $7)
         "#,
-        Uuid::new_v4(),
-        body.get_display_name(),
-        body.first_name,
-        body.last_name,
-        body.get_email(),
-        body.phone_number,
-        &body.role as &UserRole,
+        new_user.id,
+        new_user.display_name.as_ref(),
+        new_user.first_name.as_ref(),
+        new_user.last_name.as_ref(),
+        new_user.email.as_ref(),
+        new_user.phone_number,
+        &new_user.user_role as &UserRole,
     )
     .execute(pool.get_ref())
     .await
