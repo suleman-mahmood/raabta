cdc0dcfc86ab33f530283fb66ec68e6f0da65beb
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-10-17 22:58:18 +0500

    done with cookie and jwt auth

--- src/routes/admin_portal/login.rs
+++ src/routes/admin_portal/login.rs
@@ -2,8 +2,13 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use actix_web::{get, post, web, HttpRequest, HttpResponse};
 use askama::Template;
-use jsonwebtoken::{encode, EncodingKey, Header};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
+
+// TODO: Move these to env or config
+const JWT_SECRET: &str = "secret_af";
+const ADMIN_EMAIL: &str = "admin@raabta.com";
+const ADMIN_PASS: &str = "root";
 
 #[derive(Template)]
 #[template(path = "login.html")]
@@ -13,6 +18,18 @@ struct LoginTemplate {}
 async fn login(req: HttpRequest) -> HttpResponse {
     if let Some(token_cookie) = req.cookie("token") {
         log::info!("Got token cookie {}", token_cookie.value());
+        if let Ok(token_data) = decode::<Claims>(
+            token_cookie.value(),
+            &DecodingKey::from_secret(JWT_SECRET.to_string().as_ref()),
+            &Validation::default(),
+        ) {
+            let claims = token_data.claims;
+            log::info!("Got claims: {:?}", claims);
+
+            if claims.user_role == UserRoleAdminPortal::Admin {
+                log::info!("Admin found!")
+            }
+        }
     }
     HttpResponse::Ok().body(LoginTemplate {}.render().unwrap())
 }
@@ -31,39 +48,53 @@ struct SubmitLoginFormData {
     password: String,
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+enum UserRoleAdminPortal {
+    Admin,
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
     exp: usize,
-    sub: String,
+    user_role: UserRoleAdminPortal,
+}
+
+fn create_cookie() -> Option<(String, String)> {
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).ok()?;
+    let thirty_mins = Duration::new(30 * 60, 0);
+    let token_expiry = usize::try_from(now.as_secs() + thirty_mins.as_secs()).ok()?;
+    let claims = Claims {
+        exp: token_expiry,
+        user_role: UserRoleAdminPortal::Admin,
+    };
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(JWT_SECRET.to_string().as_ref()),
+    )
+    .ok()?;
+
+    Some((
+        "Set-Cookie".to_string(),
+        format!(
+            "token={}; Max-Age={}; Secure; HttpOnly; Path=/; SameSite=Strict;",
+            token,
+            thirty_mins.as_secs()
+        ),
+    ))
 }
 
 #[post("/submit-login")]
 async fn submit_login(body: web::Form<SubmitLoginFormData>) -> HttpResponse {
-    // TODO: Move these credentials to env variables or config files
-    if body.email == "admin@raabta.com" && body.password == "root" {
-        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-        let thirty_mins = Duration::new(30 * 60, 0);
-        let claims = Claims {
-            exp: usize::try_from(now.as_millis() + thirty_mins.as_millis()).unwrap(),
-            sub: "admin".to_string(),
-        };
-        let token = encode(
-            &Header::default(),
-            &claims,
-            &EncodingKey::from_secret("secret_af".as_ref()), // TODO: Move this to env
-        )
-        .unwrap();
-
-        HttpResponse::Ok()
-            .insert_header((
-                "Set-Cookie",
-                format!(
-                    "token={}; Max-Age={}; Secure; HttpOnly; Path=/",
-                    token,
-                    thirty_mins.as_secs()
-                ),
-            ))
-            .body(LoginSuccessTemplate {}.render().unwrap())
+    if body.email == ADMIN_EMAIL && body.password == ADMIN_PASS {
+        if let Some(cookie) = create_cookie() {
+            log::info!("Created a cookie successfully, {:?}", cookie);
+            HttpResponse::Ok()
+                .insert_header(cookie)
+                .body(LoginSuccessTemplate {}.render().unwrap())
+        } else {
+            HttpResponse::Ok().body(LoginSuccessTemplate {}.render().unwrap())
+        }
     } else {
         HttpResponse::Ok().body(WrongCredentialsTemplate {}.render().unwrap())
     }
