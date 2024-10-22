14fd6c4461f6b152f5630351d6cbcc1a5b79dfe1
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-10-22 19:33:58 +0500

    default route, added login link with some css

--- src/admin_portal/routes/default_route.rs
+++ src/admin_portal/routes/default_route.rs
@@ -1,6 +1,11 @@
 use actix_web::{get, HttpResponse, Responder};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct DefaultTemplate {}
 
 #[get("/")]
 async fn default() -> impl Responder {
-    HttpResponse::Ok().body("Welcome to Raabta!")
+    HttpResponse::Ok().body(DefaultTemplate {}.render().unwrap())
 }
