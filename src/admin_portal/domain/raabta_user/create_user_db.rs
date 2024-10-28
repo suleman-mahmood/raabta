6ed89d870e48837b24db5b72a9457bea064b4896
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-10-28 19:33:30 +0500

    remove redundant model file

--- src/admin_portal/domain/raabta_user/create_user_db.rs
+++ src/admin_portal/domain/raabta_user/create_user_db.rs
@@ -48,3 +48,32 @@ impl CreateUser {
         }
     }
 }
+
+// enum RaabtaUser {
+//     Student(Student),
+//     Parent(UserData),
+//     Teacher(UserData),
+//     SchoolAdmin(SchoolAdmin),
+// }
+
+// struct UserData {
+//     id: String,
+//     display_name: String,
+//     first_name: String,
+//     last_name: String,
+//     email: String,
+//     phone_number: Option<String>,
+// }
+
+// struct Student {
+//     uesr_data: UserData,
+//     parent_user_id: String,
+// }
+
+// struct SchoolAdmin {
+//     id: String,
+//     display_name: String,
+// }
+
+// impl RaabtaUser {}
+//
