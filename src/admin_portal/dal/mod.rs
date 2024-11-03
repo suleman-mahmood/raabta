c59d30173cbd67d38bdeeff2a20275a12b83fdf5
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-11-03 13:34:58 +0500

    added db functions

--- src/admin_portal/dal/mod.rs
+++ src/admin_portal/dal/mod.rs
@@ -1,2 +1,4 @@
 pub mod class_db;
+pub mod id_map_db;
+pub mod membership_db;
 pub mod user_db;
--- src/admin_portal/routes/membership_route.rs
+++ src/admin_portal/routes/membership_route.rs
@@ -2,6 +2,8 @@ use actix_web::{post, web, HttpResponse};
 use serde::Deserialize;
 use sqlx::PgPool;
 
+use crate::admin_portal::membership_db;
+
 #[derive(Deserialize)]
 struct UserClassFormData {
     user_id: String,
@@ -13,7 +15,10 @@ pub async fn add_user_to_class(
     body: web::Form<UserClassFormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    todo!();
+    match membership_db::add_user_to_class(&body.user_id, &body.class_id, &pool).await {
+        Ok(_) => todo!(),
+        Err(_) => todo!(),
+    }
 }
 
 #[post("/remove-user-class")]
@@ -21,5 +26,8 @@ pub async fn remove_user_from_class(
     body: web::Form<UserClassFormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    todo!();
+    match membership_db::remove_user_from_class(&body.user_id, &body.class_id, &pool).await {
+        Ok(_) => todo!(),
+        Err(_) => todo!(),
+    }
 }
