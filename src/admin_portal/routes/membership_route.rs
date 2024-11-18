81dcb4b6b127c6d17c654bb2a78d6ddba64d5f8e
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-11-18 14:24:24 +0500

    added templates

--- src/admin_portal/routes/membership_route.rs
+++ src/admin_portal/routes/membership_route.rs
@@ -12,22 +12,22 @@ struct UserClassFormData {
 
 #[post("/add-user-class")]
 pub async fn add_user_to_class(
-    body: web::Form<UserClassFormData>,
+    body: web::Query<UserClassFormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
     match membership_db::add_user_to_class(&body.user_id, &body.class_id, &pool).await {
-        Ok(_) => todo!(),
-        Err(_) => todo!(),
+        Ok(_) => HttpResponse::Ok().body("Added class to user!"),
+        Err(_) => HttpResponse::Ok().body("Failed to add user to user!"),
     }
 }
 
 #[post("/remove-user-class")]
 pub async fn remove_user_from_class(
-    body: web::Form<UserClassFormData>,
+    body: web::Query<UserClassFormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
     match membership_db::remove_user_from_class(&body.user_id, &body.class_id, &pool).await {
-        Ok(_) => todo!(),
-        Err(_) => todo!(),
+        Ok(_) => HttpResponse::Ok().body("Removed class from user!"),
+        Err(_) => HttpResponse::Ok().body("Failed to remove user from user!"),
     }
 }
