b254c3fb2518783f15ba3a4e60a8c6166cb423a8
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-04-09 12:44:57 +0500

    fix homework id in domain model

--- src/domain/homework.rs
+++ src/domain/homework.rs
@@ -1,14 +1,12 @@
 use chrono::serde::ts_seconds;
 use chrono::{DateTime, Utc};
 use serde::Serialize;
-use uuid::Uuid;
 
 use crate::routes::api::homework_route::CreateHomeworkBody;
 use crate::utils;
 
 pub struct CreateHomework {
-    pub id: Uuid,
-    pub public_id: String,
+    pub id: String,
     pub teacher_user_id: String,
     pub title: String,
     pub prompt: String,
@@ -30,8 +28,7 @@ impl TryFrom<CreateHomeworkBody> for CreateHomework {
     type Error = String;
     fn try_from(value: CreateHomeworkBody) -> Result<Self, Self::Error> {
         Ok(Self {
-            id: Uuid::new_v4(),
-            public_id: utils::generate_public_id(),
+            id: utils::generate_public_id(),
             teacher_user_id: value.teacher_user_id,
             title: value.title,
             prompt: value.prompt,
