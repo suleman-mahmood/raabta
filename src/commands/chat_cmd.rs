9cb29658ae5bb914096c8c6aaf5106a5a597f78b
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-04-16 12:43:39 +0500

    use ?

--- src/commands/chat_cmd.rs
+++ src/commands/chat_cmd.rs
@@ -20,21 +20,12 @@ pub async fn send_message(msg: ChatMessageCreateDTO, pool: &PgPool) -> anyhow::R
     }
 
     let chat_id = if common_chats.is_empty() {
-        match chat_db::create_chat(&msg.sender_id, &msg.recipient_id, "", pool).await {
-            Ok(cid) => cid,
-            Err(e) => {
-                log::error!("Error creating chat: {:?}", e);
-                bail!(e);
-            }
-        }
+        chat_db::create_chat(&msg.sender_id, &msg.recipient_id, "", pool).await?
     } else {
         common_chats.first().unwrap().to_owned()
     };
 
-    if let Err(e) = chat_db::send_message(&msg.message, &msg.sender_id, &chat_id, pool).await {
-        log::error!("Error inserting chat message: {:?}", e);
-        bail!(e);
-    }
+    chat_db::send_message(&msg.message, &msg.sender_id, &chat_id, pool).await?;
 
     Ok(())
 }
